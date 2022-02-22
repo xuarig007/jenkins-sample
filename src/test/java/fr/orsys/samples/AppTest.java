3b0390a562130a8ca248f0428df7ff4a1950c529
XUARIG <35503724+xuarig007@users.noreply.github.com>
2022-02-22 10:23:13 +0100

    Update AppTest.java

--- src/test/java/fr/orsys/samples/AppTest.java
+++ src/test/java/fr/orsys/samples/AppTest.java
@@ -33,7 +33,7 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( false);
+        assertTrue( true);
     }
     
     
@@ -44,5 +44,14 @@ public class AppTest
     {
         assertTrue( true);
     }
+    
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp2()
+    {
+        assertTrue( false);
+    }
+    
     
 }
